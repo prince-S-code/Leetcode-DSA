b17666287d106a388724e11786e77382afd1891a
Prince Shukla <shuklavg800@gmail.com>
2026-03-05 20:24:39 +0530

    Time: 225 ms (79.44%), Space: 8.8 MB (69.5%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,55 +1,68 @@
 class Solution {
-    bool issafe(vector<vector<char>>& board, int row, int col, char val) {
-        // check row
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == val) {
-                return false;
-            }
-        }
-        // check col
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == val) {
+    bool issafe(vector<vector<char>>& board, int row, int col,char val){
+        // checking row
+        for(int i=0;i<9;i++){
+            if(board[row][i]==val){
                 return false;
             }
         }
 
-        // check 3*3 board
-        int r = (row / 3) * 3;
-        int c = (col / 3) * 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[r + i][c + j] == val) {
+        // checking columns
+        for(int i=0;i<9;i++){
+            if(board[i][col]==val){
+                return false;
+            }
+        }
+
+        // checking 3 cross 3 grid
+        int row_base=3*(row/3);
+        int col_base=3*(col/3);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(board[row_base+i][col_base+j]==val){
                     return false;
                 }
             }
         }
-        return true;
+        return true; // in case all the condition checks are true;
     }
 
-    bool solve(vector<vector<char>>& board, int row,int col) {
-        int nextrow = row;
-        int next_col = col + 1;
-        if (row == 9) {
+    bool solve(vector<vector<char>>& board,int row,int col){
+        // base cases and modification of row and column indexes 
+        if(row==9){
             return true;
         }
-        if (next_col == 9) {
-            next_col = 0;
-            nextrow = row + 1;
+        int next_row=row;
+        int next_col=col+1;
+
+        if(next_col==9){
+            next_col=0;
+            next_row=row+1;
         }
+
+        // the case when the element is already filled one
         if(board[row][col]!='.'){
-            return solve(board,nextrow,next_col);
+            return solve(board,next_row,next_col);
         }
-        for (int val = 1; val <= 9; val++) {
-            if(issafe(board,row,col,'0'+val)){
-                board[row][col] = '0'+val;
-                if(solve(board, nextrow, next_col)){
+        
+        // now the case when there is an empty space
+        // tryin out all the possible values
+        for(int i=1;i<=9;i++){
+            char val='0'+i;
+            if(issafe(board,row,col,val)){
+                board[row][col]=val;
+                // i have done it without if returning the value of solve for next row and next col which is wrong
+                if(solve(board,next_row,next_col)){
                     return true;
                 }
-                board[row][col] = '.';
+                board[row][col]='.'; // backtracking
+                // return solve(board,next_row,next_col);
+                // the above return statement is wrong because the function wont check again for the other values and there will be no backtracking
             }
         }
         return false;
     }
+
 
 public:
     void solveSudoku(vector<vector<char>>& board) {
