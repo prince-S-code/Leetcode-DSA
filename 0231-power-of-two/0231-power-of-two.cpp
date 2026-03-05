3a806f3b4d8ab68c598e511f90ede7de3c5da98d
Prince Shukla <shuklavg800@gmail.com>
2026-03-05 20:43:05 +0530

    Time: 0 ms (100%), Space: 7.8 MB (49.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==1){
-            return true;
-        }
-        if(n<=0 || n%2!=0){
-            return false;
-        }
-        return isPowerOfTwo(n/2);
+        if(n<=0) return false;
+        return !(n&(n-1));
     }
 };
