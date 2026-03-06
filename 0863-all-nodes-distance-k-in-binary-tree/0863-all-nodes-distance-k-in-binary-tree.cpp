8a15206e50c540a02ac280254ef1a95fa8cdd63b
Prince Shukla <shuklavg800@gmail.com>
2026-03-07 03:12:41 +0530

    Time: 241 ms (7.64%), Space: 99.9 MB (5.35%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,71 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
+    void node_finder(TreeNode* curr,int k,set<TreeNode*>& st,vector<int>& ans,unordered_map<TreeNode*,TreeNode*> child_parent){
+        if(k==0){
+            ans.push_back(curr->val);
+            return;
+        }
+        if(curr->left && st.count(curr->left)==0){
+            st.insert(curr->left);
+            node_finder(curr->left,k-1,st,ans,child_parent);
+        }
+        if(curr->right && st.count(curr->right)==0){
+            st.insert(curr->right);
+            node_finder(curr->right,k-1,st,ans,child_parent);
+        }
+        if(child_parent[curr] && st.count(child_parent[curr])==0){
+            st.insert(child_parent[curr]);
+            node_finder(child_parent[curr],k-1,st,ans,child_parent);
+        }
+    }
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        // child to parent linking
-        // mpp[child] = parent of the child 
-        unordered_map<TreeNode*, TreeNode*> mpp;
-        queue<TreeNode*>parent;
-        parent.push(root);
-        while(parent.size()){
-            TreeNode* curr=parent.front();
-            parent.pop();
-            if(curr->left){
-                parent.push(curr->left);
-                mpp[curr->left]=curr;
-            }
-            if(curr->right){
-                parent.push(curr->right);
-                mpp[curr->right]=curr;
-            }
-        }
-
-        // bfs from the target to outwards direction not visited ones;
-        set<TreeNode*> visited;
+        // do level order traversal for making the unordered_mapping of child to parent
         queue<TreeNode*> q;
-        q.push(target);
-        visited.insert(target);
-        int dis=0;
-        while(q.size()){
-            if(dis==k){
-                break;
-            }
-            dis++;
-            int currsize=q.size();
-            for(int i=0;i<currsize;i++){
+        unordered_map<TreeNode*,TreeNode*> child_parent;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
                 TreeNode* curr=q.front();
                 q.pop();
-                // left
-                if(curr->left && !visited.count(curr->left)){
+                if(curr->left){
                     q.push(curr->left);
-                    visited.insert(curr->left);
-                    
+                    child_parent[curr->left]=curr;
                 }
-                // right
-                if(curr->right && !visited.count(curr->right)){
+                if(curr->right){
                     q.push(curr->right);
-                    visited.insert(curr->right);
-                }
-                // parent
-                if(mpp.count(curr) && !visited.count(mpp[curr])){
-                    q.push(mpp[curr]);
-                    visited.insert(mpp[curr]);
+                    child_parent[curr->right]=curr;
                 }
             }
         }
+        set<TreeNode*> visited;
+        visited.insert(target);
         vector<int> ans;
-        while(q.size()){
-            ans.push_back(q.front()->val);
-            q.pop();
-        }
+        node_finder(target,k,visited,ans,child_parent);
         return ans;
     }
 };
