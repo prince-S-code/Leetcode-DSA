ffef312b639efce99831a7b4e73248c8f5f2e176
Prince Shukla <shuklavg800@gmail.com>
2026-03-20 01:02:11 +0530

    Time: 3 ms (61.62%), Space: 10.2 MB (75.78%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,49 +1,52 @@
 class Solution {
-    bool issafe(vector<string>&board,int row,int col){
-        // checkign row
-        for(int i=0;i<row;i++){
-            if(board[i][col]=='Q'){
-                return false;
-            }
-        }
-        // checkign col
+    bool issafe(int row,int col,vector<string>& board){
+        int n=board.size();
+        // row check
         for(int i=0;i<col;i++){
             if(board[row][i]=='Q'){
                 return false;
             }
         }
-        // primary diagonal
-        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+        // column check
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        // primary diagonal check
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
             if(board[i][j]=='Q'){
                 return false;
             }
         }
-        // secondary diagonal
-        for(int i=row-1,j=col+1;i>=0 && j<board.size();i--,j++){
+        // secondary diagonal check
+        for(int i=row,j=col;i>=0 && j<n;i--,j++){
             if(board[i][j]=='Q'){
                 return false;
             }
         }
         return true;
     }
-    void solve(vector<string>& board,int row,int n,vector<vector<string>>& ans){
+
+    void solve(vector<string>& board,int row,vector<vector<string>>& ans){
+        int n=board.size();
         if(row==n){
             ans.push_back(board);
             return;
         }
         for(int col=0;col<n;col++){
-            if(issafe(board,row,col)){
+            if(issafe(row,col,board)){
                 board[row][col]='Q';
-                solve(board,row+1,n,ans);
+                solve(board,row+1,ans);
                 board[row][col]='.';
             }
         }
     }
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n,string(n,'.'));
+        vector<string>board(n,string(n,'.'));
         vector<vector<string>> ans;
-        solve(board,0,n,ans);
+        solve(board,0,ans);
         return ans;
     }
 };
