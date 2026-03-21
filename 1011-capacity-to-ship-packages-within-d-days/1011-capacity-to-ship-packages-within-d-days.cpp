a161cdef0eee33cbf571965d3ac53f61ed8ebe62
Prince Shukla <shuklavg800@gmail.com>
2026-03-21 11:41:37 +0530

    Time: 13 ms (42.5%), Space: 35 MB (87.09%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    bool is_possible(vector<int>& weights,int guess,int days){
+        int days_req=0;
+        int curr_sum=0;
+        for(int i=0;i<weights.size();i++){
+            if(curr_sum+weights[i] <= guess){
+                curr_sum+=weights[i];
+            }else{
+                curr_sum=weights[i];
+                days_req++;
+            }
+        }
+        if(curr_sum) days_req++;
+        if(days_req <=days){
+            return true;
+        }
+        return false;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low=*max_element(weights.begin(),weights.end());
+        int high=accumulate(weights.begin(),weights.end(),0);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(is_possible(weights,mid,days)){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
