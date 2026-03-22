5651f3c0b614443e635aafc7e955eb580f011fd1
Prince Shukla <shuklavg800@gmail.com>
2026-03-22 18:24:50 +0530

    Time: 7 ms (73.49%), Space: 29 MB (91.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -11,28 +11,27 @@ public:
                 if(grid[i][j]==1){
                     if(i==0 || j==0 || i==m-1 || j==n-1){ // boundary locations
                         q.push({i,j});
-                        grid[i][j]=2;
                     }else{
                         ones++;
                     }
                 }
             }
         }
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
+        // vector<vector<bool>>visited(m,vector<bool>(n,false));
         vector<pair<int,int>> directions={{0,1},{0,-1},{1,0},{-1,0}};
         while(!q.empty()){
             auto current=q.front();
             q.pop();
             int x=current.first; 
             int y=current.second;
-            visited[x][y]=true;
+            // visited[x][y]=true;
             for(auto direction : directions){
                 int nx=x+direction.first;
                 int ny=y+direction.second;
-                if(nx<m-1 && nx>0 && ny>0 && ny<n-1 && grid[nx][ny]==1 && !visited[nx][ny]){
+                if(nx<m-1 && nx>0 && ny>0 && ny<n-1 && grid[nx][ny]==1){
                     ones--;
                     q.push({nx,ny});
-                    visited[nx][ny]=true;
+                    grid[nx][ny]=0;
                 }
             }
         }
