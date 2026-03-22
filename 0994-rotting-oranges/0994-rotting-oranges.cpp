80b261f595da3158535879fff8241cd1618b3eb2
Prince Shukla <shuklavg800@gmail.com>
2026-03-22 15:08:56 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.12%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,41 +3,39 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        queue<pair<int,int>> q;
         int fresh=0;
-        // pushing the rotten indexes to the queue for the points to act as the source and counting the no of fresh ones
-        for(int row=0;row<m;row++){
-            for(int col=0;col<n;col++){
-                if(grid[row][col]==1){
+        queue<pair<int,int>> q;
+        // pushing the rotten indexes to the queue and counting the fresh ones
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }else if(grid[i][j]==1){
                     fresh++;
-                }else if(grid[row][col]==2){
-                    q.push({row,col});
                 }
             }
         }
-
-        // multisource bfs
-        int time=0;
+        int minutes=0;
         vector<pair<int,int>> directions={{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.empty() && fresh>0){
             int size=q.size();
-            time++;
+            minutes++;
             for(int i=0;i<size;i++){
                 auto current=q.front();
                 q.pop();
-                int row=current.first;
-                int col=current.second;
+                int x=current.first;
+                int y=current.second;
                 for(auto direction:directions){
-                    int new_row=row+direction.first;   
-                    int new_col=col+direction.second;
-                    if(new_row < m && new_row >=0 && new_col<n && new_col>=0 && grid[new_row][new_col]==1){
-                        grid[new_row][new_col]=2;
+                    int nx=x+direction.first;
+                    int ny=y+direction.second;
+                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1){
                         fresh--;
-                        q.push({new_row,new_col});
+                        grid[nx][ny]=2;
+                        q.push({nx,ny});
                     }
                 }
             }
         }
-        return fresh==0?time:-1;
+        return fresh==0?minutes:-1;
     }
 };
