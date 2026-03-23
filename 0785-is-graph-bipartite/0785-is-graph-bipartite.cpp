6385903b91e05f75e74eab5dfa6fe16bc65e38f9
Prince Shukla <shuklavg800@gmail.com>
2026-03-23 17:06:25 +0530

    Time: 0 ms (100%), Space: 17.4 MB (58.02%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,30 +1,35 @@
 class Solution {
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& color) {
+        for (int neighbour : adj[node]) {
+            // If not colored → assign opposite color
+            if (color[neighbour] == 0) {
+                color[neighbour] = -color[node];
+                if (!dfs(neighbour, adj, color)) {
+                    return false;
+                }
+            }
+            // If already colored and same → conflict
+            else if (color[neighbour] == color[node]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> color(n,-1);
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){//uncolored
-                queue<int> q;
-                q.push(i);
-                color[i]=0;
-                while(!q.empty()){
-                    int size=q.size();
-                    for(int i=0;i<size;i++){
-                        int node=q.front();
-                        q.pop();
-                        for(int neighbour:graph[node]){
-                            if(color[neighbour]==-1){
-                                q.push(neighbour);
-                                color[neighbour]=1^color[node];
-                            }else if(color[neighbour]==color[node]){
-                                return false;
-                            }
-                        }
-                    }
+        int n = graph.size();
+        vector<int> color(n, 0); // 0 = unvisited
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == 0) {
+                color[i] = 1;  // start coloring
+                if (!dfs(i, graph, color)) {
+                    return false;
                 }
             }
         }
+
         return true;
     }
 };
