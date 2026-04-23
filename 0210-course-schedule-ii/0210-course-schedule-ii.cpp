71a64066ca6cbade05b10c1b5017203e15461c12
Prince Shukla <shuklavg800@gmail.com>
2026-04-23 20:06:32 +0530

    Time: 3 ms (71.66%), Space: 18.6 MB (44.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,39 +1,45 @@
 class Solution {
-    bool dfs(vector<vector<int>> adj, int node,vector<int>& visited, vector<int>& path_visited,vector<int>&ans){
-        visited[node]=1;
-        path_visited[node]=1;
-        for(auto it:adj[node]){
-            if(!visited[it]){
-                visited[it]=1;
-                path_visited[it]=1;
-                if(dfs(adj,it,visited,path_visited,ans)){
+    bool dfs(vector<vector<int>>& adj, int node,
+             vector<int>& visited, vector<int>& path,
+             vector<int>& ans) {
+
+        visited[node] = 1;
+        path[node] = 1;
+
+        for (auto it : adj[node]) {
+            if (!visited[it]) {
+                if (dfs(adj, it, visited, path, ans))
                     return true;
-                }
-            }else if(path_visited[it]){
-                return true;
+            } 
+            else if (path[it]) {
+                return true; // cycle
             }
         }
-        path_visited[node]=0;
+
+        path[node] = 0;
         ans.push_back(node);
         return false;
     }
+
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> ans;
-        int n=numCourses;
-        vector<int> visited(n ,0);
-        vector<int> path_visited(n ,0);
-        vector<vector<int>> adj(n);
-        for(auto elem:prerequisites){
-            adj[elem[1]].push_back(elem[0]);
+        vector<vector<int>> adj(numCourses);
+
+        for (auto &p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
         }
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                if(dfs(adj,i,visited,path_visited,ans))
+
+        vector<int> visited(numCourses, 0), path(numCourses, 0);
+        vector<int> ans;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (dfs(adj, i, visited, path, ans))
                     return {};
             }
         }
-        reverse(ans.begin(),ans.end());
-        return ans; 
+
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
