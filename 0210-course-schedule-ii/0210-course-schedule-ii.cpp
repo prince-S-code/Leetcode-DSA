d96dd622631d7f38e98c59b7c5a0b20546f49f3f
Prince Shukla <shuklavg800@gmail.com>
2026-04-23 20:20:50 +0530

    Time: 2 ms (75.97%), Space: 18.4 MB (56.24%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,45 +1,36 @@
 class Solution {
-    bool dfs(vector<vector<int>>& adj, int node,
-             vector<int>& visited, vector<int>& path,
-             vector<int>& ans) {
-
-        visited[node] = 1;
-        path[node] = 1;
-
-        for (auto it : adj[node]) {
-            if (!visited[it]) {
-                if (dfs(adj, it, visited, path, ans))
-                    return true;
-            } 
-            else if (path[it]) {
-                return true; // cycle
-            }
-        }
-
-        path[node] = 0;
-        ans.push_back(node);
-        return false;
-    }
-
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-
-        for (auto &p : prerequisites) {
-            adj[p[1]].push_back(p[0]);
+        int n=numCourses;
+        vector<int> indegree(n,0);
+        vector<int> visited(n,0);
+        vector<vector<int>> adj(n);
+        for(auto & it : prerequisites){
+            indegree[it[0]]++;
+            adj[it[1]].push_back(it[0]);
         }
-
-        vector<int> visited(numCourses, 0), path(numCourses, 0);
-        vector<int> ans;
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (dfs(adj, i, visited, path, ans))
-                    return {};
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-
-        reverse(ans.begin(), ans.end());
+        vector<int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            visited[node]=1;
+            ans.push_back(node);
+            for(auto& neighbour:adj[node]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+        }
+        if(ans.size()!=n){
+            return {};
+        }
         return ans;
     }
 };
