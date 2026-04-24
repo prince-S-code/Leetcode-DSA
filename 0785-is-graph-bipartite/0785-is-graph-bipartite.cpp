7b71be16a9a061c9c70f85d4bce8343561ef7395
Prince Shukla <shuklavg800@gmail.com>
2026-04-24 12:23:30 +0530

    Time: 0 ms (100%), Space: 17.6 MB (41.87%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,35 +1,33 @@
 class Solution {
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& color) {
-        for (int neighbour : adj[node]) {
-            // If not colored → assign opposite color
-            if (color[neighbour] == 0) {
-                color[neighbour] = -color[node];
-                if (!dfs(neighbour, adj, color)) {
-                    return false;
-                }
-            }
-            // If already colored and same → conflict
-            else if (color[neighbour] == color[node]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n, 0); // 0 = unvisited
-
-        for (int i = 0; i < n; i++) {
-            if (color[i] == 0) {
-                color[i] = 1;  // start coloring
-                if (!dfs(i, graph, color)) {
-                    return false;
+        // using bfs
+        // vector<int> visited(n,0);
+        // there is no need for visited array the color array will have the uncolored nodes which tells about visited and unvisited
+        int n=graph.size();
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++){// for loop is for the unconnected graph has it been connected graph there would have been no need of this for loop
+            if(color[i]==-1){ // that is the node is uncolored and unvisited
+                queue<int> q;
+                q.push(i); 
+                color[i]=1;//initialising it with any random color, 0 would have also worked
+                while(!q.empty()){
+                    int size=q.size();// to know how many nodes to process in the single cycle
+                    for(int i=0;i<size;i++){
+                        int node=q.front();
+                        q.pop();
+                        for(int & u:graph[node]){
+                            if(color[u]==-1){
+                                color[u]=1^color[node];
+                                q.push(u);
+                            }else if(color[u]==color[node]){
+                                return false;
+                            }
+                        }
+                    }
                 }
             }
         }
-
         return true;
     }
 };
