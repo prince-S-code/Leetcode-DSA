fd4c65346a91bfe9ee9d6f9f7aa7f02d86abe785
Prince Shukla <shuklavg800@gmail.com>
2026-04-24 16:19:32 +0530

    Time: 55 ms (66.04%), Space: 21.2 MB (42.45%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
+    // using bfs
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> st(wordList.begin(),wordList.end());
-        st.erase(beginWord);
-        queue<pair<string,int>> q;
+        queue<pair<string,int>> q;//the word and the no of steps to reach that word
+        int n=beginWord.size();
         q.push({beginWord,1});
         while(!q.empty()){
             auto current=q.front();
             q.pop();
-            string word= current.first;
+            string word=current.first;
             int steps=current.second;
-            if(word== endWord){
+            if(word==endWord){
                 return steps;
             }
-            for(int i=0;i<word.size();i++){
+            for(int i=0;i<n;i++){//index
                 char original=word[i];
-                for(char chr='a';chr<='z';chr++){
-                    word[i]=chr;
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
                     if(st.count(word)!=0){
                         st.erase(word);
                         q.push({word,steps+1});
                     }
+                    word[i]=original;
                 }
-                word[i]=original;
             }
         }
-        // if no sequesnce exists
         return 0;
     }
 };
